14653aa17fb59bae6be7ccbe7a61b59ab5273f5b
Diego <g0d13@outlook.com>
2021-12-19 02:08:02 -0600

    prepare to push

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { MachineModule } from './modules/machine/machine.module';
+import { RequestModule } from './modules/request/request.module';
+import { LogModule } from './modules/log/log.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MachineModule, RequestModule, LogModule],
   controllers: [AppController],
   providers: [AppService],
 })
